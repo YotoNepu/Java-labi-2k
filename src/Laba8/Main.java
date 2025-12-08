6b48a31696e80417c80fb029734361b229593e75
YotoNepu <gkola772@gmail.com>
2025-12-09 02:22:58 +0300

    Rework 6 [8 almost ready] (09.12)

--- src/Laba8/Main.java
+++ src/Laba8/Main.java
@@ -9,7 +9,9 @@ public class Main {
         task1(scanner);
         task2(scanner);
         task3(scanner);
+        task4(scanner);
         task5(scanner);
+        task6(scanner);
     }
 
     public static String getData(Scanner sc, String msg) {
@@ -72,6 +74,40 @@ public class Main {
         System.out.printf("Кол-во трёхбуквенных слов: %d%n", count);
     }
 
+    public static void task4(Scanner scan) {
+        String password = getData(scan, "\nВведите пароль: ");
+
+        if (password.length() < 8 || password.length() > 12) {
+            System.out.println("Пароль не подходит: длина должна быть от 8 до 12 символов");
+            return;
+        }
+
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasSpecialChar = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            if (Character.isUpperCase(ch) && Character.isLetter(ch)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(ch) && Character.isLetter(ch)) {
+                hasLowerCase = true;
+            } else if ("#@$%\"&*<>,.!?;:".indexOf(ch) != -1) {
+                hasSpecialChar = true;
+            }
+        }
+
+        if (hasUpperCase && hasLowerCase && hasSpecialChar) {
+            System.out.println("Пароль хороший!");
+        } else {
+            System.out.println("Пароль плохой по следующим причинам:");
+            if (!hasUpperCase) System.out.println("- отсутствуют заглавные латинские буквы");
+            if (!hasLowerCase) System.out.println("- отсутствуют прописные латинские буквы");
+            if (!hasSpecialChar) System.out.println("- отсутствуют специальные символы или знаки препинания");
+        }
+    }
+
     // Я собрал за сегодня 10 закладок из 15 книг в библиотеке за 30 минут.
     public static void task5(Scanner scan) {
         int sum = 0;
@@ -86,5 +122,28 @@ public class Main {
         }
 
         System.out.printf("Сумма чисел в предложении: %d%n", sum);
+    }
+
+    public static void task6(Scanner scan) {
+        System.out.println("\nВведите текст (несколько предложений), для завершения ввода введите пустую строку:");
+
+        StringBuilder textBuilder = new StringBuilder();
+        while (true) {
+            String line = scan.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            textBuilder.append(line).append(" ");
+        }
+        String text = textBuilder.toString().trim();
+        String[] sentences = text.split("[.!?]\\s+");
+
+        String keyword = getData(scan, "Введите ключевое слово: ");
+        System.out.println("Предложения с ключевым словом:");
+        for (String sentence: sentences) {
+            if (sentence.matches(".*(^|[\\s\"()-])" + keyword + "($|[\\s,\"):;-]).*")) {
+                System.out.println(sentence);
+            }
+        }
     }
 }
